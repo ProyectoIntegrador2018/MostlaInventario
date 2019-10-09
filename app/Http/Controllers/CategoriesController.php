686ce6b851cc1541bdd1d63ee211e3834eca75c4
fedehuguet <fede.huguet14@gmail.com>
2019-10-09 16:03:57 -0500

    Crear controller de categorias

---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoriesController extends Controller
+{
+    public function index()
+    {
+        //$categoriesIndex = $this->category->allForUser();
+
+        return view('profile.categories.index');//->with(compact('categoriesIndex'));
+    }
+
+    public function create()
+    {
+        return view('profile.categories.create');
+    }
+
+    public function edit($categoryId)
+    {
+        //$categoriesEdit = $this->category->findId($categoryId);
+        
+        return view('profile.categories.edit');//->with(compact('categoriesEdit'));
+    }
+}
