14bc212327cba7bdd8012b50b47447e06235caaf
Clarissa Miranda <clarissamiranda@MacBook-Pro-de-C.local>
2019-10-23 19:24:53 -0500

    controlador catalogo

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -4,25 +4,28 @@ namespace App\Http\Controllers;
 
 use App\Repositories\ProductRepository;
 use App\Repositories\CategoryRepository;
+use App\Repositories\TagRepository;
 use Auth;
 use Illuminate\Http\Request;
 
 class CatalogController extends Controller
 {
-    private $products, $categories;
+    private $products, $categories, $tags;
 
-    public function __construct(ProductRepository $products, CategoryRepository $categories)
+    public function __construct(ProductRepository $products, CategoryRepository $categories, TagRepository $tags)
     {
         $this->products = $products;
         $this->categories = $categories;
+        $this->tags = $tags;
     }
 
     public function index()
     {
         $products = $this->products->allForUser(auth()->user());
         $categories = $this->categories->allForUser();
+        $tags = $this->tags->allForUser();
         
-        return view('profile.catalog')->with(compact('products','categories'));
+        return view('profile.catalog')->with(compact('products','categories','tags'));
     }
 
 
