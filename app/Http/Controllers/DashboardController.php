1a16a702072dfbd1dc58e6dabd4b31aa16f59223
JawnF <a01251219@itesm.mx>
2019-11-19 18:44:45 -0600

    Dashboard (#80)
    * Hacer vista y controlador de reservaciones, con opcion para cambiar reservaciones de status y cancelarlas, y buscar dentro del dashboard
    
    * Arreglar cambiar de status de inactivo a activo
    
    * Agregar iconos en cards de reservaciones

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Reservation;
+use App\Repositories\ReservationRepository;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    private $reservations;
+
+    public function __construct(ReservationRepository $reservations)
+    {
+        $this->reservations = $reservations;
+    }
+
+    public function index(Request $request)
+    {
+        $reservations = Reservation::forCampus(auth()->user()->campus_id)
+            ->with('product', 'user')
+            ->when($request->status, function ($query, $status) {
+                if ($status == 'returned' || $status == 'cancelled') {
+                    $query = $query->withTrashed();
+                }
+                return $query->where('status', $status);
+            })
+            ->get();
+        $statuses = Reservation::statuses();
+        $filters = $request->all();
+
+        return view('dashboard.index')->with(compact('reservations', 'statuses', 'filters'));
+    }
+
+    public function remind()
+    {
+        // Mandar email a usuarios cuyas reservaciones se terminan en un dia, el mismo dia
+        // o que ya hayan expirado y siguen sin regresar.
+
+        return back()->with('alert', 'Se han enviado los recordatorios.');
+    }
+}
