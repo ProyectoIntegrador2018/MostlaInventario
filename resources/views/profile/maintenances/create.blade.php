3ee05074457db983069a3dc2a4872f9d2a78affd
clarismiranda <52264247+clarismiranda@users.noreply.github.com>
2019-11-26 11:10:29 -0600

    Agrega mantenimento (#90)
    * comentario y vistas
    
    * cambia redirect
    
    * crear mantenimiento

---
+++ resources/views/profile/maintenances/create.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+
+<section>
+
+	<h1>Agregar mantenimiento</h1>
+	<a href="{{ url()->previous() }}">< Regresar</a>
+	<br>
+	<form class="inline-form" action="/maintenances/store" method="POST">
+		@csrf
+		Product:<br>
+        <select disabled class="selectpicker form-control" name="product_id">
+            <option value="{{$unitCreated->product->id}}">{{$unitCreated->product->name}}</option>
+        </select>
+		Unit:<br>
+        <select disabled class="selectpicker form-control" name="unit_id">
+            <option value="{{$unitCreated->id}}">{{$unitCreated->serial_number}}</option>
+        </select>
+        <br>
+		<div>
+			Comentario:<br>
+			<textarea class="form-control" name="comment"></textarea>
+			<br>
+		</div>
+		<button type="submit" class="btn btn-primary">Crear</button>
+	</form>
+	</section>
+@endsection
